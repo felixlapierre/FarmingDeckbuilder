c52b51bf3a0ed94022b3b32760ab3dbd044e7b35
Felix Lapierre <felix.lapierre@appdirect.com>
2024-07-08 16:02:54 -0400

    Fix a bug where plants harvested by harvester don't trigger on harvest effects

--- src/structure/custom/harvester.gd
+++ src/structure/custom/harvester.gd
@@ -16,7 +16,9 @@ func register_events(event_manager: EventManager, tile: Tile):
 		for target_tile in args.farm.get_all_tiles():
 			if Helper.is_nearby(target_tile.grid_location, tile.grid_location)\
 				and target_tile.state == Enums.TileState.Mature:
-				target_tile.harvest(false)
+				var effects = target_tile.harvest(false)
+				args.farm.effect_queue.append_array(effects)
+		args.farm.process_effect_queue()
 	event_manager.register_listener(event_type, callback)
 
 func unregister_events(event_manager: EventManager):
