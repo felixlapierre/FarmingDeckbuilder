d5eee77bb46a714c48f36848c74f8d909e4ec56d
Felix <felix.lapierre1@gmail.com>
2025-01-15 21:53:56 -0500

    Fix a bug where fire flower will softlock the game if used with 0 cards in hand. Also if used with 1 card in hand, just burn that card without prompting the player to choose

--- src/cards/custom/fire_flower.gd
+++ src/cards/custom/fire_flower.gd
@@ -8,14 +8,21 @@ var event_type = EventManager.EventType.AfterCardPlayed
 
 func register_events(event_manager: EventManager, p_tile: Tile):
 	callback = func(args: EventArgs):
-		var pick_option_ui = PickOption.instantiate()
-		args.user_interface.add_child(pick_option_ui)
-		var prompt = "Pick a card to Burn"
+		var hand_info = args.cards.get_hand_info()
+		
+		if hand_info.size() > 1:
+			var pick_option_ui = PickOption.instantiate()
+			args.user_interface.add_child(pick_option_ui)
+			var prompt = "Pick a card to Burn"
 
-		pick_option_ui.setup(prompt, args.cards.get_hand_info(), func(selected):
-			args.cards.remove_card_with_info(selected)
-			args.cards.notify_card_burned(selected)
-			args.user_interface.remove_child(pick_option_ui), null)
+			pick_option_ui.setup(prompt, hand_info, func(selected):
+				args.cards.remove_card_with_info(selected)
+				args.cards.notify_card_burned(selected)
+				args.user_interface.remove_child(pick_option_ui), null)
+		elif hand_info.size() == 1:
+			var burned = hand_info[0]
+			args.cards.remove_card_with_info(burned)
+			args.cards.notify_card_burned(burned)
 	event_manager.register_listener(event_type, callback)
 
 func unregister_events(event_manager: EventManager):
